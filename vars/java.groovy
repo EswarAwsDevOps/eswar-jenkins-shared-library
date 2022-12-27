8c192123399d4b10bfa415b03a1b03c4946434d7
eswar <eswar.awsdevops@gmail.com>
2022-12-27 16:20:53 +0530

    updating ./eswar-jenkins-shared-library

---
+++ vars/java.groovy
@@ -0,0 +1,23 @@
+def call() {
+node {
+
+    stage('Compile') {
+        echo 'Compile'
+    }
+
+            stage('Code Quality') {
+                echo 'Code Quality'
+                    }
+
+            stage('Test Cases') {
+                echo 'Test Cases'
+                }
+
+            stage('Publish A Release') {
+                echo 'Publish A Release'
+            }
+
+        }
+
+    }
+
---
+++ vars/nodejs.groovy
@@ -0,0 +1,19 @@
+def call() {
+    node {
+
+        stage('Code Quality') {
+            echo 'Code Quality'
+        }
+
+        stage('Test Cases') {
+            echo 'Test Cases'
+        }
+
+        stage('Publish A Release') {
+            echo 'Publish A Release'
+        }
+
+    }
+
+}
+
