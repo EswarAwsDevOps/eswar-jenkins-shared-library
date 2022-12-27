b343218690b2e90f5277a1ec2e036ca712124c08
eswar <eswar.awsdevops@gmail.com>
2022-12-27 19:07:21 +0530

    updating ./eswar-jenkins-shared-library

--- vars/common.groovy
+++ vars/common.groovy
@@ -3,5 +3,28 @@ def checkout(){
         cleanWs()
         git branch: 'main', url: "${env.REPO_URL}"
     }
+}
+def compile(appType){
+
+    stage('compile code'){
+        if(appType == "java"){
+            sh 'mvn clean compile'
+        }
+ if(appType == "golang"){
+     sh 'go and init'
+ }
+    }
+}
+
+
+def Code Quality(){
+    stage('Code Qulaity'){
+        echo 'Code Quality'
+    }
+}
+def Release(){
+    stage('Publish A Release'){
+        echo 'Publish A Release'
+    }
 
 }
--- vars/java.groovy
+++ vars/java.groovy
@@ -1,23 +1,9 @@
 def call() {
-   node {
-       common.checkout()
-            stage('Compile') {
-                echo 'Compile'
-                    }
-           stage('Code Quality') {
-                echo 'Code Quality'
-
-       }
-
-            stage('Test Cases') {
-                echo 'Test Cases'
-                }
-
-            stage('Publish A Release') {
-                echo 'Publish A Release'
-            }
-
-        }
-
+    node {
+        common.checkout()
+        common.compile()
+        common.CodeQuality()
+        common.release()
     }
 
+}
