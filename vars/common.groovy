44dea18b25131f2981dd8802f105b92a71751c10
eswar <eswar.awsdevops@gmail.com>
2022-12-28 15:57:38 +0530

    cleanup

--- vars/common.groovy
+++ vars/common.groovy
@@ -18,7 +18,7 @@ def compile(appType) {
 
 def CodeQuality() {
     stage('Code Quality') {
-        sh "-Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.host.url=http://172.31.7.220:9000 -Dsonar.projectKey=${env.COMPONENT} "
+        sh "sonar-scanner -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.host.url=http://172.31.7.220:9000 -Dsonar.projectKey=${env.COMPONENT} "
 //        "sonar-scanner -Dsonar.qualitygate.wait=true -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.host.url=http://172.31.8.238:9000 -Dsonar.projectKey=${env.COMPONENT} ${SONAR_OPTS}"
         sh 'echo OK'
     }
