4e46a6047baead7a147cde8bcc001e75b3a8d5e8
eswar <eswar.awsdevops@gmail.com>
2022-12-28 18:17:57 +0530

    dns

--- vars/java.groovy
+++ vars/java.groovy
@@ -1,15 +1,15 @@
 def call() {
 //    if (!env.SONAR_OPTS) {
-//        env.SONAR_OPTS = ""
-    }
+//        env.SONAR_OPTS = "
     node {
         try {
             common.checkout()
             common.compile("java")
-            common.codeQuality()
+            common.CodeQuality()
 //            common.testCases("java")
 //            if(env.TAG_NAME ==~ ".*") {
             common.release("java")
+        } catch (e) {
         }
-    } catch (e) {
-        }
+     }
+ }
