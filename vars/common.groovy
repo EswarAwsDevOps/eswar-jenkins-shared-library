59fd042f7383f13cbc81ce1acf4fc9b64be1ead5
eswar <eswar.awsdevops@gmail.com>
2022-12-28 13:49:15 +0530

    updating ./eswar-jenkins-shared-library

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,58 +1,31 @@
-def checkout() {
-    stage('Checkout Code') {
+def checkout(){
+    stage('Checkout Code'){
         cleanWs()
         git branch: 'main', url: "${env.REPO_URL}"
     }
 }
+def compile(appType){
 
-def compile(appType) {
-
-    stage('Compile Code') {
-        if(appType == "java") {
+    stage('compile code'){
+        if(appType == "java"){
             sh 'mvn clean compile'
         }
-
-        if(appType == "golang") {
-            sh 'go mod init'
-        }
+ if(appType == "golang"){
+     sh 'go mod init'
+ }
     }
-
-
 }
-
-def testCases(appType) {
-
-    stage('Unit Tests') {
-        if(appType == "java") {
-            sh 'mvn test || true'
-        }
-
-        if(appType == "nodejs") {
-            sh 'npm test || true'
-        }
-
-        if(appType == "python") {
-            sh 'python3 -m unittest *.py || true'
-        }
-
-    }
-
-
-}
-
 
 def codeQuality() {
     stage('Code Quality') {
-        //sh "sonar-scanner -Dsonar.qualitygate.wait=true -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.host.url=http://172.31.8.238:9000 -Dsonar.projectKey=${env.COMPONENT} ${SONAR_OPTS}"
+        sh "-Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.host.url=http://172.31.7.220:9000 -Dsonar.projectKey=${env.COMPONENT} ${SONAR_OPTS}"
+//        "sonar-scanner -Dsonar.qualitygate.wait=true -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.host.url=http://172.31.8.238:9000 -Dsonar.projectKey=${env.COMPONENT} ${SONAR_OPTS}"
         sh 'echo OK'
     }
 }
+def Release(){
+    stage('Publish A Release'){
+        echo 'Publish A Release'
+    }
 
-def release(appType) {
-    stage('Publish A Release') {
-        if (appType == "nodejs") {
-            sh '''
-        npm install 
-        zip -r ${COMPONENT}-${TAG_NAME}.zip node_modules server.js schema
-      '''
-        }
+}
