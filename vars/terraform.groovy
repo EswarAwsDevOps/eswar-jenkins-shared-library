e733762ed1fd2a0273314d8f901fc01700e0551f
eswar <eswar.awsdevops@gmail.com>
2022-12-31 12:43:13 +0530

    roboshop-infra

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -1,2 +1,37 @@
+def call () {
+    pipeline {
+        agent any
 
+
+        options {
+            ansiColor('xterm')
+        }
+
+        parameters {
+            choice(name: 'ENVIRONMENT', choices: ['', 'dev', 'prod'], description: 'Pick Environment')
+            choice(name: 'ACTION', choices: ['', 'apply', 'destroy'], description: 'Pick Terraform Action')
+        }
+
+        stagegs {
+
+            stage('Terraform Init') {
+                steps {
+                    sh '''
+                        terraform init -backend-config=env/${ENVIRONMENT}-backend.tfvars
+                      '''
+                }
+            }
+
+            stage('Terraform Plan') {
+                steps {
+                    sh '''
+                        terraform plan -var-file=env/${ENVIRONMENT}.tfvars
+                      '''
+                }
+            }
+
+
+        }
+    }
+}
 
