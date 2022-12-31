57043bcad188e7b83fbff8c88991b7c1ffb71d33
eswar <eswar.awsdevops@gmail.com>
2022-12-31 12:08:03 +0530

    roboshop-infra

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -1,24 +1,52 @@
 def call() {
-    pipeline {
+    node {
 
-        agent any
+        properties([
+                parameters([
+                        [$class     : 'ChoiceParameterDefinition',
+                         choices    : '\ndev\nprod\n',
+                         name       : 'ENVIRONMENT',
+                         description: "Choose Environment"
+                        ],
+                        [$class     : 'ChoiceParameterDefinition',
+                         choices    : '\napply\ndestroy\n',
+                         name       : 'ACTION',
+                         description: "Choose Action"
+                        ],
+                ]),
+        ])
 
-//
-//        options {
-//            ansiColor('xterm')
-//        }
+        ansiColor('xterm') {
+            common.checkout()
 
-        stages {
-
-            stage ('Terraform Plan') {
-                steps {
-                    sh '''
-                        terraform plan
-                '''
-                }
+            if (!ENVIRONMENT) {
+                env.ENVIRONMENT = "${env.ENVIRONMENT_DEFAULT}"
             }
+
+            if (!ACTION) {
+                env.ACTION = "${env.ACTION_DEFAULT}"
+            }
+
+            stage('Terraform init') {
+                addShortText background: '#FFFF00', borderColor: '#FFFF00', color: '', link: '', text: "Env : ${ENVIRONMENT} | Action : ${ACTION}"
+                sh '''
+        terraform init -backend-config=env/${ENVIRONMENT}-backend.tfvars
+      '''
+            }
+
+            stage('Terraform Plan') {
+                sh '''
+        terraform plan -var-file=env/${ENVIRONMENT}.tfvars
+      '''
+            }
+
+            stage('Terraform Apply') {
+                input message: 'Apply ?', ok: 'Yes'
+                sh '''
+        terraform ${ACTION} -auto-approve -var-file=env/${ENVIRONMENT}.tfvars
+      '''
+            }
+
         }
-
     }
-
 }
