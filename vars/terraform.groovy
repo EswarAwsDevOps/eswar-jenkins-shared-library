2a68125400e26f43e221a49dd515b5cdc90c6e59
eswar <eswar.awsdevops@gmail.com>
2023-01-09 17:46:27 +0530

    creating mutable app

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -28,7 +28,7 @@ def call () {
             }
 
             stage('Terraform Init') {
-//                addShortText background: '#FFFF00', borderColor: '#FFFF00', color: '', link: '', text: "Env : ${ENVIRONMENT} | Action : ${ACTION}"
+                addShortText background: '#FFFF00', borderColor: '#FFFF00', color: '', link: '', text: "Env : ${ENVIRONMENT} | Action : ${ACTION}"
                 sh '''
               terraform init -backend-config=env/${ENVIRONMENT}-backend.tfvars
             '''
